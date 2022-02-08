7f66f2e1142f17cf2f4b8321c6c14e2a323ebddf
Matt Richardson <matt.richardson@octopus.com>
2022-02-08 15:56:05 +1100

    Upgrade to nuke 6.0.1 (#5)
    Co-authored-by: Corey Underdown <corey.underdown@octopus.com>

--- build/Build.cs
+++ build/Build.cs
@@ -1,11 +1,12 @@
+// ReSharper disable RedundantUsingDirective
 using Nuke.Common;
 using Nuke.Common.Execution;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.OctoVersion;
 using Nuke.Common.Utilities.Collections;
-using Nuke.OctoVersion;
-using OctoVersion.Core;
+using Serilog;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
@@ -13,12 +14,21 @@ using static Nuke.Common.Tools.DotNet.DotNetTasks;
 [UnsetVisualStudioEnvironmentVariables]
 class Build : NukeBuild
 {
+    const string CiBranchNameEnvVariable = "OCTOVERSION_CurrentBranch";
+
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
     [Solution] readonly Solution Solution;
 
-    [NukeOctoVersion] readonly OctoVersionInfo OctoVersionInfo;
+    [Parameter("Whether to auto-detect the branch name - this is okay for a local build, but should not be used under CI.")] 
+    readonly bool AutoDetectBranch = IsLocalBuild;
+    
+    [Parameter("Branch name for OctoVersion to use to calculate the version number. Can be set via the environment variable " + CiBranchNameEnvVariable + ".", Name = CiBranchNameEnvVariable)]
+    string BranchName { get; set; }
+
+    [OctoVersion(BranchParameter = nameof(BranchName), AutoDetectBranchParameter = nameof(AutoDetectBranch))] 
+    public OctoVersionInfo OctoVersionInfo;
 
     AbsolutePath SourceDirectory => RootDirectory / "source";
     AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
@@ -51,8 +61,8 @@ class Build : NukeBuild
         .DependsOn(Restore)
         .Executes(() =>
         {
-            Logger.Info("Building Octopus.UnwantedMethodCallsAnalyzer v{0}", OctoVersionInfo.FullSemVer);
-            Logger.Info("Informational Version {0}", OctoVersionInfo.InformationalVersion);
+            Log.Information("Building Octopus.UnwantedMethodCallsAnalyzer v{0}", OctoVersionInfo.FullSemVer);
+            Log.Information("Informational Version {0}", OctoVersionInfo.InformationalVersion);
 
             DotNetBuild(_ => _
                 .SetProjectFile(Solution)
